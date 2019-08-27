f221802d058808efa5c398af49c7968c985bdd2d
Lu <lu@hooli.store>
2019-08-27 18:05:41 +0200

    splash page css fixed for desktop

--- src/pages/SplashPage.js
+++ src/pages/SplashPage.js
@@ -6,14 +6,14 @@ require('typeface-roboto');
 export default class SplashPage extends Component {
     render() {
         return (
-            <div className="container text-center">
+            <div className="container text-center align-items">
                 <img className="logo" src="../images/logo.png" alt=" listen and talklogo"/>
                 <Link to = '/signup'><button className="margin-button">Signup</button></Link>
                 <Link to = '/login'><button className="margin-button">Connect</button></Link>
-                <p className="text-end">A social network, without image or videos</p>
-                <p className="text-end">So we can heard each other</p>
-                <p className="text-end">And really connect and engage</p>
-                <p className="text-end">Share your voice with the world, in less than a minute.</p>
+                <p>A social network, without image or videos</p>
+                <p>So we can heard each other</p>
+                <p>And really connect and engage</p>
+                <p>Share your voice with the world, in less than a minute.</p>
             </div>
         )
     }
